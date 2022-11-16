62a8173858fe54c4fe22380039c372e00ce321d2
Timothy3773 <jonathanscott122@gmail.com>
2022-11-16 07:50:28 -0500

    Update

---
+++ dataEntry/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Entry
+{
+    public string EntryID { get; set; }
+    public string EntryType { get; set; }
+}
+
+public class EntryManager
+{
+    List<Entry> entries = new List<Entry>();
+
+    public void GetEntries()
+    {
+        if (entries != null)
+        {
+            return entries.Count;
+        }else{
+            entries.Add("Ok");
+            return entries.Count;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Scott's Data Entry System");
+        Console.WriteLine("Please state your ID: ");
+        string inputId = Console.ReadLine();
+        Console.WriteLine("Validating {0}", inputId);
+        EntryManager test = new EntryManager();
+    }
+    
+}
